209d3286e01b58c36326e60aa665674bcbf7adf7
EduardoAraujo047 <you@example.com>
2021-05-06 16:01:45 -0300

    Laço de repetição WHILE

---
+++ src/br/com/senai/LacoDeRepeticaoWhile.java
@@ -0,0 +1,19 @@
+package br.com.senai;
+
+public class LacoDeRepeticaoWhile {
+
+	public static void main(String[] args) {
+		
+		int valor = 0;
+		
+		System.out.println("Valor: "+ valor);
+		
+		while(valor <= 25) {
+			System.out.println("-> Valor: "+ valor);
+			valor++;
+		}
+		
+		System.out.println("Agora valor: "+ valor);
+	}
+
+}
